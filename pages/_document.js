4670aced35f9709f73f4022e4dcf9378940cfd69
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-06 19:35:54 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,6 @@ class MyDocument extends Document {
 				<Head>
 					<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&display=swap" rel="stylesheet"/>
 					<link href="https://fonts.googleapis.com/css?family=Cabin:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet" />
-					<link rel="icon" type="image/png" href="/images/favicon.png"></link>
 				</Head>
 				<body>
 					<Main />
