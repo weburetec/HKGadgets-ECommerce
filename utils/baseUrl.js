3be5df319e6618e9e9551550745cc6c4ac1261b9
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-06 18:21:02 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
 ? 'https://hkgadgets.onrender.com' 
-: 'http://localhost:3000';
+: 'https://hkgadgets.onrender.com';
 
 export default baseUrl;
