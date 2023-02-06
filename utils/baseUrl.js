49ee1aaadcfb95b691fed49a72d016085e51b4dd
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-06 14:53:26 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://livani-react.envytheme.com' 
+? 'https://hkgadgets.onrender.com' 
 : 'http://localhost:3000';
 
 export default baseUrl;
