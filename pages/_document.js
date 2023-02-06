f44fa9320b159af1596e6f5cb7ee186bc3d6d30a
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-06 21:33:23 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
             <Html lang="zxx">
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <link rel="icon" type="image/png" href={require("../images/favicon.png")}></link>
+
                 </Head>
                 <body>
                     <Main />
