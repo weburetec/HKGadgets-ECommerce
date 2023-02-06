40f8519fd657feb862271988f701aba1dc8d618d
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-06 20:10:11 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,7 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&display=swap" rel="stylesheet"/>
-					<link href="https://fonts.googleapis.com/css?family=Cabin:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet" />
-				</Head>
+
 				<body>
 					<Main />
 					<NextScript />
