b1a4f92c439cdf513ba14c061ce87ecd8a754f84
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-07 16:08:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const path = require("path");
+const webpack = require("webpack");
+console.log(webpack.version);
 
 module.exports = {
+	 webpack: function (config, options) {
+    		console.log(options.webpack.version); // 4.44.1
+   		config.experiments = {};
+    		return config;
+  	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")],
 	},
