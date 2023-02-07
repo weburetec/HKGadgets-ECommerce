504439228543ffebf84c68eed46a281a04ca23ba
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-07 12:47:54 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,12 +11,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>HK Gadgets</title>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
+            <meta charSet="UTF-8" />
+      	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
         <body>
           <Main />
