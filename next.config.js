3ce9817da0891091e8f32f5dae6a9d9b8ec65334
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-07 17:57:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const path = require("path");
 module.exports = {
 	webpack: (config) => {
 		const webpack = require('webpack')
-		const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-		config.plugins = config.plugins || []
-		config.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en/))
-		config.plugins.push(new BundleAnalyzerPlugin())
-		return config
 	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")],
