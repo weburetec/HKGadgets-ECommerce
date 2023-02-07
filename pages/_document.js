9e7a7bbc92093b6d7e8020b2b265271c823b2c43
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-07 12:07:46 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="zxx">
-                <Head>
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
 
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+    return initialProps;
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
